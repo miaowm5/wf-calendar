ffd8597a13bf5a70e30a2b0e6fced773b8da4161
miaowm5 <miaowm5@gmail.com>
2021-10-17 12:30:15 +0800

    change alarm time rule

--- parseData.js
+++ parseData.js
@@ -1,4 +1,13 @@
 
+const getAlarmTime = (time)=>{
+  let hours = time.getHours()
+  if (hours <= 12){
+    return hours + 4
+  }else{
+    return hours - 8
+  }
+}
+
 const parse = (item)=>{
   const prop = item.properties
 
@@ -7,7 +16,6 @@ const parse = (item)=>{
   const tag = prop.Tags.select.name
   let title = `[${tag}]${prop.Name.title[0].plain_text}`
   let description = `弹射世界${server}日程提醒：\n${title}`
-  let alarm = false
 
   const timeDate = [
     time.getUTCFullYear(),
@@ -17,20 +25,20 @@ const parse = (item)=>{
     time.getUTCMinutes()
   ]
 
-  if (tag === '活动结束'){ alarm = true }
-
   const result = {
     startInputType: 'utc',
     start: timeDate,
     end: timeDate,
     title,
     description,
+    alarms: [
+      {
+        action: 'display',
+        description: 'Reminder',
+        trigger: { hours: getAlarmTime(time), minutes: 0, before: true }
+      }
+    ]
   }
-
-  if (alarm){
-    result.alarms = [{action: 'display', description: 'Reminder', trigger: { hours: 6, minutes: 0, before: true }}]
-  }
-
   return result
 }
 
