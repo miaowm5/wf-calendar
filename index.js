3c055edfa66c5c08c8c7d5dc65028b427b70c92f
miaowm5 <miaowm5@gmail.com>
2021-10-31 01:04:35 +0800

    remove error handle

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const generate = require('./generate')
 
 const main = async ()=>{
   const list = await getData()
-  if (!list){ return }
   const event = list.reduce((r, item)=>r.concat(parseData(item)), [])
   await generate(event)
 }
