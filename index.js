94aa7033796b3e7b8177a62c2674cbee5f52dcb0
miaowm5 <miaowm5@gmail.com>
2021-10-31 00:40:15 +0800

    support reminder event start

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const generate = require('./generate')
 const main = async ()=>{
   const list = await getData()
   if (!list){ return }
-  const event = list.map(parseData)
+  const event = list.reduce((r, item)=>r.concat(parseData(item)), [])
   await generate(event)
 }
 
--- parseData.js
+++ parseData.js
@@ -9,14 +9,8 @@ const getAlarmTime = (time)=>{
   }
 }
 
-const parse = (item)=>{
-  const prop = item.properties
-
-  const time = new Date(prop.Date.date.start)
-  let server = '日服'
-  const tag = prop.Tags.select.name
-  let title = `[${tag}]${prop.Name.title[0].plain_text}`
-  let description = `弹射世界${server}日程提醒：\n${title}`
+const genReminder = (title, time)=>{
+  const server = '日服'
 
   const timeDate = [
     time.getUTCFullYear(),
@@ -26,12 +20,12 @@ const parse = (item)=>{
     time.getUTCMinutes()
   ]
 
-  const result = {
+  return {
     startInputType: 'utc',
     start: timeDate,
     end: timeDate,
     title,
-    description,
+    description: `弹射世界${server}：\n${title}\nhttps://wf-calendar.miaowm5.com/`,
     alarms: [
       {
         action: 'display',
@@ -39,6 +33,22 @@ const parse = (item)=>{
         trigger: { hours: getAlarmTime(time), minutes: 0, before: true }
       }
     ]
+  }
+}
+
+const parse = (item)=>{
+  const prop = item.properties
+  const timeStart = prop.StartDate.date ? new Date(prop.StartDate.date.start) : null
+  const timeEnd = prop.Date.date ? new Date(prop.Date.date.start) : null
+  const tag = prop.Tags.select.name
+  const title = prop.Name.title[0].plain_text
+  const result = []
+  if (timeStart && timeEnd){
+    result.push(genReminder(`[${tag}开始]${title}`, timeStart))
+    result.push(genReminder(`[${tag}结束]${title}`, timeEnd))
+  }else{
+    if (timeStart){ result.push(genReminder(`[${tag}]${title}`, timeStart)) }
+    if (timeEnd){ result.push(genReminder(`[${tag}]${title}`, timeEnd)) }
   }
   return result
 }
