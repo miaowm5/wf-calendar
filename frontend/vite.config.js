c181d571439ce7cd7b077f2e762ed8145b7fc647
miaowm5 <miaowm5@gmail.com>
2022-01-23 14:41:48 +0800

    add proxy info

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+const proxy = 'https://wf-calendar.miaowm5.com'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   build: {
     outDir: '../dist/frontend'
+  },
+  server: {
+    proxy: {
+      '/data': { target: proxy, changeOrigin: true },
+      '/banner': { target: proxy, changeOrigin: true }
+    }
   }
 })
