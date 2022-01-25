a65b487731db67c922b44217f6a436923c098245
miaowm5 <miaowm5@gmail.com>
2022-01-25 10:24:34 +0800

    remove store

---
+++ frontend/src/getInfo.js
@@ -0,0 +1,69 @@
+
+const handleItem = (info)=>{
+  const today = new Date()
+  const event = { ...info }
+  const { timeEnd, timeStart } = event
+
+  // 设置用于排序的时间
+  event.timeSort = new Date(timeEnd || timeStart)
+
+  // 设置活动状态
+  if (event.timeSort - today < 0){
+    // 时间已过：已经开始/已经结束
+    event.status = timeEnd ? 'end' : 'open'
+  }else{
+    // 时间未过
+    // 存在开始时间
+    if (timeStart){
+      // 开始时间已过：等待结束
+      if (new Date(timeStart) - today < 0){
+        event.status = 'progress'
+      }else{
+        // 开始时间未过：等待开始
+        event.status = 'before'
+        event.timeSort = new Date(timeStart)
+      }
+    }else{
+      // 不存在开始时间：等待结束
+      event.status = 'progress'
+    }
+  }
+
+  // 对于还未开始/结束的活动，计算剩余时间
+  if (['progress', 'before'].includes(event.status)){
+    event.remain = (event.timeSort - today) / 1000
+  }
+  return event
+}
+
+const handleServer = (info)=>{
+  const server = { ...info }
+  server.list = server.list
+    .map((info)=>handleItem(info))
+    .sort((a, b)=>{
+      const sort = a.timeSort - b.timeSort
+      if (sort !== 0){ return sort }
+      const statusSort = { 'end': 0, 'open': 1, 'before': 2, 'progress': 3 }
+      return (statusSort[a.status] || 0) - (statusSort[b.status] || 0)
+    })
+  return server
+}
+
+const handleData = (text)=>{
+  const result = JSON.parse(text)
+  result.list = result.list.map((info)=>handleServer(info))
+  return result
+}
+
+const getInfo = async ()=>{
+  const res = await fetch(`/data/info.json`)
+  const text = await res.text()
+  if (res.ok){
+    return handleData(text)
+  }else{
+    console.error(text)
+    throw new Error(text)
+  }
+}
+
+export default getInfo
--- frontend/src/index.svelte
+++ frontend/src/index.svelte
@@ -1,20 +1,16 @@
 <script>
   import './reset.css'
-  import data from './store/data'
   import Server from './server.svelte'
   import Time from './time.svelte'
+  import getInfo from './getInfo'
 
-  const getInfo = async ()=>{
-    const res = await fetch(`/data/info.json`)
-    const text = await res.text()
-    if (res.ok){
-      data.set(JSON.parse(text))
-    }else{
-      console.error(text)
-      throw new Error(text)
-    }
+  let data = {}
+  let promise
+  const load = async ()=>{
+    const result = await getInfo()
+    data = result
   }
-  const promise = getInfo()
+  promise = load()
 
 </script>
 
@@ -22,10 +18,10 @@
   <p class="hint">读取数据日历列表中...</p>
 {:then}
   <header></header>
-  {#each $data.list as server(server.id)}
+  {#each data.list as server(server.id)}
     <Server server={server} />
   {/each}
-  <Time date={$data.time} />
+  <Time date={data.time} />
 {:catch}
   <p class="hint">日历列表读取失败</p>
 {/await}
