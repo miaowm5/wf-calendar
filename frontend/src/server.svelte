16ef547da8652b3288baa6972f4451dd24b2931a
miaowm5 <miaowm5@gmail.com>
2022-02-03 18:15:40 +0800

    combine notion button to server

--- frontend/src/server.svelte
+++ frontend/src/server.svelte
@@ -1,23 +1,23 @@
 <script>
   export let server
-  import Button from './button.svelte'
   import Item from './item/index.svelte'
 </script>
 
-<div class="main">
-  <Button title={server.server} url={server.notion} />
-  {#each server.list as item(item.id)}
-    <Item {item} server={server.flag} />
-  {/each}
-</div>
+<a class="button" href="{server.notion}" target="_blank">Notion 日历</a>
+{#each server.list as item(item.id)}
+  <Item {item} server={server.flag} />
+{/each}
 
 <style>
-  .main {
-    max-width: 1000px;
-    margin: 0 auto;
-    padding: 0 1em;
-  }
-  @media (max-width: 460px){
-    .main{ padding: 0 .5em; }
+  .button{
+    font-size: .9em;
+    color: white;
+    background-color: rgb(42, 42, 42);
+    margin: 1em 0;
+    text-align: center;
+    padding: .8em;
+    display: block;
+    text-decoration: none;
+    border-radius: 5px;
   }
 </style>
