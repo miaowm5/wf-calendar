0f7666835fa6f096f643ea3ecfa627ac3a7ad5d2
Monkey Juice <miaowm5@gmail.com>
2024-02-23 10:34:00 +0000

    support check dir info update

--- deploy/checkUpdate.js
+++ deploy/checkUpdate.js
@@ -11,15 +11,15 @@ const loadCache = async (cachePath)=>{
 // 比较数据相比上一次部署是否更新
 const checkUpdate = async (generate, target)=>{
   const update = {
-    list: {},
-    cache: {},
-    update: false
+    list: {},      // 记录当前版本相较此前版本的更新列表，用于处理后续图片数据如何更新
+    cache: {},     // 记录当前版本所有数据的更新时间，供下次部署时比较
+    update: false, // 此次更新检查的最终结果
   }
   const server = await fs.readJson(path.join(generate, 'deploy/info.json'))
   const cachePath = path.join(target, '_cache.json')
   const cache = await loadCache(cachePath)
 
-  server.forEach(({ flag, list, header, footer })=>{
+  server.forEach(({ flag, list, die, header, footer })=>{
     const updateList = {}
     const lastEdit = cache[flag] || {}
     const newEdit = {}
@@ -34,9 +34,12 @@ const checkUpdate = async (generate, target)=>{
       }
       newEdit[id] = edit
     }
+    // 按照ID依序比较数据更新时间
     list.forEach((item)=>check(item.id, item.edit))
+    if (die){ die.forEach((item)=>check(item.id, item.edit)) }
     check('header', header.edit)
     check('footer', footer.edit)
+    // 比较缓存数据长度以及当前数据长度，从而处理数据删除的情况
     if (Object.keys(lastEdit).length !== Object.keys(newEdit).length){
       update.update = true
     }
